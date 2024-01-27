277f5b5728d13c8df9ea61ed9ef631d6e246f928
Veshal Raj <veshalraj1307@gmail.com>
2024-01-27 14:33:04 +0530

    recaptcha in backend

---
+++ src/infrastructureLayer/middlewares/reCaptcha.ts
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch';
+import { Request, Response, NextFunction } from 'express';
+require('dotenv').config()
+
+interface GoogleResponse {
+    success: boolean;
+}
+
+async function recaptcha(req: Request, res: Response, next: NextFunction) {
+    console.log(req.body);
+
+    // TO-DO: Replace the token and reCAPTCHA action variables before running the sample.
+    
+    const recaptchaKey = process.env.recaptchaKey;
+    const token = req.body?.token || req.body?.Token;
+
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaKey}&response=${token}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'post'
+        });
+
+        const google_response = await response.json() as GoogleResponse;
+
+        console.log(google_response.success);
+
+        if (google_response.success) {
+            next();
+        } else {
+            res.status(403).json("Error");
+        }
+    } catch (error) {
+        res.json({ error });
+    }
+}
+
+export default recaptcha;
