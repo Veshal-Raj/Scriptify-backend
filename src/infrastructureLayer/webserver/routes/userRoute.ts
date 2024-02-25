72258eae4b15ced375d59256cfa43a8fec28096a
Veshal Raj <veshalraj1307@gmail.com>
2024-02-25 23:01:47 +0530

    recaptcha middleware

--- src/infrastructureLayer/webserver/routes/userRoute.ts
+++ src/infrastructureLayer/webserver/routes/userRoute.ts
@@ -1,12 +1,14 @@
 import { Route, Req, Res, Next } from "../../types/serverPackageTypes";
 import { userController } from "./injections/injections";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
+import recaptcha from "../middlewares/reCaptcha";
 
 
 export function userRoute(router: Route) {
     
     router.post(
         '/register',
+        recaptcha,
         catchAsyncErrors((req: Req, res: Res, next: Next) => {
             userController.registerUser(req, res, next)
         })
