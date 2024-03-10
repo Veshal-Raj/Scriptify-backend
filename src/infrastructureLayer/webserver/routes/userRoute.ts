16439371d3a7346ee892a8091a312db24209182c
Veshal Raj <veshalraj1307@gmail.com>
2024-03-10 18:08:52 +0530

    explore tags route

--- src/infrastructureLayer/webserver/routes/userRoute.ts
+++ src/infrastructureLayer/webserver/routes/userRoute.ts
@@ -2,53 +2,68 @@ import { Route, Req, Res, Next } from "../../types/serverPackageTypes";
 import { userController } from "./injections/injections";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
 import recaptcha from "../middlewares/reCaptcha";
-// import { nanoid } from "nanoid";
 
 export function userRoute(router: Route) {
-    
-    router.post(
-        '/register',
-        recaptcha,
-        catchAsyncErrors((req: Req, res: Res, next: Next) => {
-            userController.registerUser(req, res, next)
-        })
-    )
+  router.post(
+    "/register",
+    recaptcha,
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.registerUser(req, res, next);
+    })
+  );
 
-    router.post(
-        '/verify-otp',
-        catchAsyncErrors((req: Req, res: Res, next: Next) => {
-            userController.createUser(req, res, next)
-        })
-    )
+  router.post(
+    "/verify-otp",
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.createUser(req, res, next);
+    })
+  );
 
-    router.post(
-        '/login',
-        recaptcha,
-        catchAsyncErrors((req: Req, res: Res, next: Next) => {
-            userController.login(req, res, next)
-        })
-    )
+  router.post(
+    "/login",
+    recaptcha,
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.login(req, res, next);
+    })
+  );
 
-    router.get('/resend-otp')
+  router.get("/resend-otp");
 
-    router.get('/get-upload-url', catchAsyncErrors((req: Req, res: Res, next: Next) => {
-        userController.generateUploadURL(req, res, next)
-    }))
+  router.get(
+    "/get-upload-url",
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.generateUploadURL(req, res, next);
+    })
+  );
 
+  router.post(
+    "/create-blog",
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.createBlog(req, res, next);
+    })
+  );
 
-    router.post('/create-blog', catchAsyncErrors((req: Req,res: Res, next: Next) => {
-        userController.createBlog(req, res, next)
-    }))
-    
+  router.get(
+    "/latest-blog",
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.latestBlog(req, res, next);
+    })
+  );
 
-    router.get('/latest-blog', catchAsyncErrors((req: Req, res: Res, next: Next) => {
-        userController.latestBlog(req, res, next)
-    }))
-     
+  router.get(
+    "/trending-blog",
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.trendingBlog(req, res, next);
+    })
+  );
 
-    router.get('/trending-blog', catchAsyncErrors((req:Req, res: Res, next: Next) => {
-        userController.trendingBlog(req,res, next) 
-    }))
+  router.get(
+    "/explore-tags",
+    catchAsyncErrors((req: Req, res: Res, next: Next) => {
+      userController.exploreTags(req, res, next);
+    })
+  );
 
-    return router
+  
+  return router;
 }
