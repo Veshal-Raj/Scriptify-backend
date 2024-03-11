2b482812f4623585849c8e019d7e24cd09705b96
Veshal Raj <veshalraj1307@gmail.com>
2024-03-11 18:09:52 +0530

    filterbytags route

--- src/infrastructureLayer/webserver/routes/userRoute.ts
+++ src/infrastructureLayer/webserver/routes/userRoute.ts
@@ -64,6 +64,22 @@ export function userRoute(router: Route) {
     })
   );
 
-  
+    router.post(
+        "/filterbyTags",
+        catchAsyncErrors((req: Req, res: Res, next: Next) => {
+            userController.filterByTags(req, res, next)
+        })
+    )
+
+    // router.post('/filterByTags', (req, res) => {
+    //     let { tag } = req.body
+    //     let findTag = { tags: tag, draft: false }
+    //     let maxLimit = 5;
+    //     Blog.find(findTag)
+    //     .populate("author", "personal_info.profile_img personal_info.username -_id")
+    //         .sort({ "publishedAt": -1 })
+    //         .select("blog_id title des banner activity tags publishedAt -_id")
+    //         .limit(maxLimit);
+    // })
   return router;
 }
