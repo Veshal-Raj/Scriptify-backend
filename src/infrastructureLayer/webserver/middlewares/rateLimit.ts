51503bd8a868a3c1a77e960be97fb8dcc6da1995
Veshal Raj <veshalraj1307@gmail.com>
2024-03-11 19:13:26 +0530

    ratelimt updated

--- src/infrastructureLayer/webserver/middlewares/rateLimit.ts
+++ src/infrastructureLayer/webserver/middlewares/rateLimit.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 export  const limiter = rateLimit({
     windowMs: 60 * 1000,
-    max:50,
+    max:200,
     message: "Too many requests from this IP, please try again later.",
     headers: true,
 })
