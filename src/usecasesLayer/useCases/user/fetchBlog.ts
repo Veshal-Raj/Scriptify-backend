e7adae0e6baebdbfbf0e6e46bcf2405ce7bdb959
Veshal Raj <veshalraj1307@gmail.com>
2024-03-26 11:45:00 +0530

    useCase engine for fetch user single blog

---
+++ src/usecasesLayer/useCases/user/fetchBlog.ts
@@ -0,0 +1,21 @@
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+export const fetchBlog = async (
+    // blogRepository:
+    blog_id: string,
+    userRepository: IUserRepository,
+    next: Next,
+    logger: ILogger
+) => {
+    try {
+       console.log('reached inside the latestBlog usecase engine ')
+       const response = await userRepository.fetchSingleBlog(blog_id, next)
+       return response
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+}
