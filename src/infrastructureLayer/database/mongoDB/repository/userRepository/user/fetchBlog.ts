5ce57b33082f94b5a2bcdb37ce8b536d27c37fb8
Veshal Raj <veshalraj1307@gmail.com>
2024-03-26 11:45:44 +0530

    engine for repository for fetch single blog

---
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/fetchBlog.ts
@@ -0,0 +1,30 @@
+import UserModel from "../../../models/userModel";
+import BlogModel from "../../../models/blogModel";
+
+
+
+export const fetchBlog = async(
+    blog_id: string,
+    blogModels: typeof BlogModel, 
+) => {
+    try {
+        console.log('reached inside the repository/userRepository/user/latestBlogs');
+        // Create a new instance of BlogModel
+       console.log(blog_id)
+
+       const blog = await blogModels.findOne({ blog_id: blog_id }).populate({
+        path: 'author',
+        select: {
+            'personal_info.username': 1,
+            'personal_info.bio': 1,
+            'personal_info.profile_img': 1,
+            'social_links': 1
+        }
+    });
+        console.log(blog)
+        return blog;
+    } catch (error) {
+        console.error(error);
+        throw error
+    }
+}
