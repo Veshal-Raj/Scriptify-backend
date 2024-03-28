2b0aed7795240a5377fe17a9e786e324c5f1558f
Veshal Raj <veshalraj1307@gmail.com>
2024-03-28 12:23:56 +0530

    feat: unsave blog user repository engine added

---
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/unSavedBlogByUser.ts
@@ -0,0 +1,32 @@
+
+import UserModel from "../../../models/userModel";
+
+export const unSavedBlogByUser = async (
+    blog_id: string,
+    userId: string,
+    userModel: typeof UserModel,
+    
+): Promise<any | void> => {
+    try {
+        const user = await userModel.findById(userId);
+        if (!user) {
+            console.log('User not found.');
+            return { success: false, message: 'User not found.' };
+        }
+        console.log(user)
+        const savedBlogIndex = user.userInteractions.userSavedBlogs.findIndex(blog => String(blog.blogId) === blog_id);
+        console.log('index --- ',savedBlogIndex)
+        if (savedBlogIndex === -1) {
+            console.log('User has not saved the blog.');
+            return { success: false, message: 'User has not saved the blog.' };
+        }
+
+        // Remove the blog from the user's saved blogs list
+        user.userInteractions.userSavedBlogs.splice(savedBlogIndex, 1);
+        await user.save();
+
+        return { success: true, message: 'Blog unsaved successfully.' };
+    } catch (error) {
+        throw error
+    }
+}
