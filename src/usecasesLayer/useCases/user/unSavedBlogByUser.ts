80d41ddeb5ca7f63c4a7d7e1cc258ddfc8642641
Veshal Raj <veshalraj1307@gmail.com>
2024-03-28 12:23:24 +0530

    feat: unsave blog user usecase engine added

---
+++ src/usecasesLayer/useCases/user/unSavedBlogByUser.ts
@@ -0,0 +1,23 @@
+
+
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+export const unSavedBlogByUser = async (
+    blogId: string,
+    userId: string,
+    userRepository: IUserRepository,
+    next: Next,
+    logger: ILogger
+) => {
+    try {
+       console.log('reached inside the latestBlog usecase engine ')
+       const response = await userRepository.unSaveBlog(blogId,userId, next)
+       return response
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+}
