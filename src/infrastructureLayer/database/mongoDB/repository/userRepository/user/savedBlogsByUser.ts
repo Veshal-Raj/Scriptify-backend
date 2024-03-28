6433b50128f4afca9e61cbeaaeee8401893054b2
Veshal Raj <veshalraj1307@gmail.com>
2024-03-28 16:26:01 +0530

    feat: saved blog user repository engine added

---
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/savedBlogsByUser.ts
@@ -0,0 +1,34 @@
+
+import BlogModel from "../../../models/blogModel";
+import UserModel from "../../../models/userModel";
+
+export const savedBlogsByUser = async (
+    userId: string,
+    userModel: typeof UserModel,
+    blogModel: typeof BlogModel
+): Promise<any | void> => {
+    try {
+        const user = await userModel.findById(userId);
+        if (!user) {
+            console.log('User not found.');
+            return { success: false, message: 'User not found.' };
+        }
+        console.log(user)
+
+        const savedBlogIds = user.userInteractions.userSavedBlogs.map(blog => blog.blogId);
+
+        // Find the blog documents corresponding to the savedBlogIds
+        const savedBlogs = await blogModel.find({ _id: { $in: savedBlogIds } })
+            .select('_id blog_id title banner activity.total_likes des tags author publishedAt')
+            .populate("author", "personal_info.profile_img personal_info.username -_id")
+
+        // Return the retrieved saved blogs
+        // return savedBlogs;
+        console.log('saved blogs .....',savedBlogs)
+
+        return savedBlogs
+        
+    } catch (error) {
+        throw error
+    }
+}
