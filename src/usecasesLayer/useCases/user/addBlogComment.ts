7b91ef8e4e859fcf7c85b6697dedf969a640e3c4
Veshal Raj <veshalraj1307@gmail.com>
2024-03-31 21:48:38 +0530

    feat: added add comment usecase engine

---
+++ src/usecasesLayer/useCases/user/addBlogComment.ts
@@ -0,0 +1,24 @@
+
+
+import { Comment, CommentData } from "../../../@types/general/Comments";
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+export const addBlogComment = async (
+    commentData: CommentData,
+    comment: Comment,
+    next: Next,
+    userRepository: IUserRepository,
+    logger: ILogger
+) => {
+    try {
+       console.log('reached inside the latestBlog usecase engine ')
+       const response = await userRepository.addComment( commentData, comment, next)
+       return response
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+}
