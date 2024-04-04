a3078e73e47f924257a8cb04766728e8170a7798
Veshal Raj <veshalraj1307@gmail.com>
2024-04-04 10:32:25 +0530

    update: changed the folder name to adapters

---
+++ src/AdaptersLayer/adminController.ts
@@ -0,0 +1,32 @@
+import { Next, Req, Res } from "../infrastructureLayer/types/serverPackageTypes";
+import { IAdminUseCase } from "../usecasesLayer/interface/usecase/adminUseCase";
+
+
+
+export class AdminController {
+    private readonly adminUseCase: IAdminUseCase;
+
+    constructor(adminUseCase:IAdminUseCase) {
+        this.adminUseCase = adminUseCase
+    }
+
+    async getAllUser(req: Req, res: Res, next: Next) {
+        try {
+            const result = await this.adminUseCase.getAllUser(next);
+            // console.log('result --->> ', result)
+            res.status(200).json({ success: true, message: 'users have been fetched successfully', data: result})
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async changeUserStatus(req: Req, res: Res, next: Next) {
+        try {
+            const result = await this.adminUseCase.changeUserStatus(req, next);
+            console.log('result --->>>> ', result)
+            res.status(200).json({ success: true, message: 'user status changed successfully', data: result})
+        } catch (error) {
+            throw error
+        }
+    }
+}
