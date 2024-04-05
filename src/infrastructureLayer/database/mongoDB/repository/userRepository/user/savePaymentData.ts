f4c6ab12527c0f30b16c8ca32f9af1ffd1a52fe8
Veshal Raj <veshalraj1307@gmail.com>
2024-04-06 00:10:02 +0530

    feat: added savePaymentData in userrepository

---
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/savePaymentData.ts
@@ -0,0 +1,80 @@
+import PaymentModel from "../../../models/paymentMode";
+import UserModel from "../../../models/userModel";
+
+export const savePaymentData = async (
+  paymentMethod: string,
+  userId: string,
+  receipt_url: string,
+  subscriptionType: string
+) => {
+  try {
+    console.log("<------------------------------------->");
+    console.log("paymentMethod >>> ", paymentMethod);
+    console.log("userId >>>> ", userId);
+    console.log("reciept_url >>> ", receipt_url);
+    console.log("subscriptionType >>>> ", subscriptionType);
+    console.log("<------------------------------------->");
+    if (subscriptionType == "monthly") {
+      const user = await UserModel.findById(userId);
+      if (!user) return null;
+      const userIdFromDb = user._id;
+      const subscriptionId = paymentMethod;
+      const SubscriptionType = subscriptionType;
+      const receiptUrl = receipt_url;
+
+      const paymentRecord = new PaymentModel({
+        userId: userIdFromDb,
+        subscriptionId: subscriptionId,
+        subscriptionType: SubscriptionType,
+        subscribedDate: new Date(),
+        subscriptionExpirationDate: new Date(
+          Date.now() + 30 * 24 * 60 * 60 * 1000
+        ),
+        receipt_url: receiptUrl,
+      });
+      await paymentRecord.save();
+
+      console.log("payemnt record ---- ", paymentRecord);
+      const paymentRecordId = paymentRecord._id;
+
+      if (user) {
+        await UserModel.updateOne(
+          { _id: userId },
+          { $set: { subscriptionId: paymentRecordId } }
+        );
+      }
+    } else if (subscriptionType == "annually") {
+        const user = await UserModel.findById(userId);
+        if (!user) return null;
+        const userIdFromDb = user._id;
+        const subscriptionId = paymentMethod;
+        const SubscriptionType = subscriptionType;
+        const receiptUrl = receipt_url;
+  
+        const paymentRecord = new PaymentModel({
+          userId: userIdFromDb,
+          subscriptionId: subscriptionId,
+          subscriptionType: SubscriptionType,
+          subscribedDate: new Date(),
+          subscriptionExpirationDate: new Date(
+            Date.now() + 365 * 24 * 60 * 60 * 1000
+          ),
+          receipt_url: receiptUrl,
+        });
+        
+        await paymentRecord.save();
+  
+        console.log("payemnt record ---- ", paymentRecord);
+        const paymentRecordId = paymentRecord._id;
+  
+        if (user) {
+          await UserModel.updateOne(
+            { _id: userId },
+            { $set: { subscriptionId: paymentRecordId } }
+          );
+        }
+    } else return 
+  } catch (error) {
+    throw error;
+  }
+};
