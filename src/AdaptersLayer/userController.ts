845a82003214489e1235cc0035bd4367fc72b66e
Veshal Raj <veshalraj1307@gmail.com>
2024-04-08 17:50:57 +0530

    feat: added recipturl

--- src/AdaptersLayer/userController.ts
+++ src/AdaptersLayer/userController.ts
@@ -504,4 +504,14 @@ export class UserController {
             throw error
         }
     }
+
+    async reciptUrl(req: Req, res: Res, next: Next) {
+        try {
+            const userId = req.query.userId as string
+            const response = await this.userUseCase.reciptUrl(userId, next)
+            return res.status(200).json({ response })
+        } catch (error: unknown | never) {
+            throw error
+        }
+    }
 }
