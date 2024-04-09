19e188edae240d020cbfb3d5d082f4e2242b71e6
Veshal Raj <veshalraj1307@gmail.com>
2024-04-09 23:31:04 +0530

    feat: added getChatOfUser

---
+++ src/usecasesLayer/useCases/user/getChatOfUser.ts
@@ -0,0 +1,22 @@
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+
+
+export const getChatOfUser = async (
+    senderId: string,
+    receiverId: string,
+    userrepository: IUserRepository,
+    next: Next,
+    logger: ILogger
+) => {
+    try {
+        const response = await userrepository.getChat(senderId, receiverId)
+        return response
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+}
