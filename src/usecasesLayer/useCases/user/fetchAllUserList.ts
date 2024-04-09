29010f1a3d3f83b005396ee345e385e6f05d2b26
Veshal Raj <veshalraj1307@gmail.com>
2024-04-09 23:30:47 +0530

    feat: added fetchAllUserlist

---
+++ src/usecasesLayer/useCases/user/fetchAllUserList.ts
@@ -0,0 +1,18 @@
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+export const fetchAllUserList = async (
+    next:Next,
+    userRepository: IUserRepository,
+    logger: ILogger
+) => {
+    try {
+        const result = await userRepository.fetchAllUsers()
+        return result
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+} 
