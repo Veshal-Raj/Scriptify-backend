aa24d2d72ce53699f3db36e3a5fe298dec8d03be
Veshal Raj <veshalraj1307@gmail.com>
2024-04-09 23:21:59 +0530

    feat: added sendchatbyuser repsoitory engine

---
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/sendChatByUser.ts
@@ -0,0 +1,36 @@
+import { IConversation } from "../../../../../../@types/general/chatData"
+import ChatModel from "../../../models/chatModel"
+import Message from "../../../models/messageModel"
+
+
+export const sendChatByUser = async (
+    data: IConversation
+) => {
+    try {
+        console.log('data ------------- ', data)
+        let chat = await ChatModel.findOne({
+            users: { $all: [data.sender.id, data.receiver.id]}
+        })
+
+        if (!chat) {
+            chat = await ChatModel.create({
+                users: [data.sender.id, data.receiver.id]
+            })
+        }
+
+        const message = await Message.create({
+            sender: data.sender.id,
+            receiver: data.receiver.id,
+            content: data.message,
+            chat:chat._id
+        })
+
+        chat.latestMessage = message._id
+        await chat.save()
+
+        console.log('Chat and message created successfully');
+
+    } catch (error) {
+        throw error
+    }
+}
