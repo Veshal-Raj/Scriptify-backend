a3391f6b51c498bb17e7f2e3dadb4898e5a995fc
Veshal Raj <veshalraj1307@gmail.com>
2024-04-09 23:21:24 +0530

    feat: added getChatofuser repository engine

---
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/getChatOfUser.ts
@@ -0,0 +1,39 @@
+import ChatModel from "../../../models/chatModel";
+import Message from "../../../models/messageModel";
+
+
+
+export const getChatOfUser = async (
+    senderId: string,
+    receiverId: string
+) => {
+    try {
+        const messages = await Message.find({
+            $or: [
+                { sender: senderId, receiver: receiverId },
+                { sender: receiverId, receiver: senderId }
+            ]
+        }).sort({ createdAt: 1 }).populate('sender', 'personal_info.username personal_info.profile_img')
+          .populate('receiver', 'personal_info.username personal_info.profile_img');
+
+          const formattedMessages = messages.map((message) => ({
+            sender: {
+                id: message.sender._id,
+                name: message.sender.personal_info.username,
+                profile_img: message.sender.personal_info.profile_img
+            },
+            receiver: {
+                id: message.receiver._id,
+                name: message.receiver.personal_info.username,
+                profile_img: message.receiver.personal_info.profile_img
+            },
+            message: message.content,
+            time: message.createdAt
+        }));
+
+        console.log('formattedMessages --- ', formattedMessages);
+        return formattedMessages;
+    } catch (error) {
+        throw error
+    }
+}
