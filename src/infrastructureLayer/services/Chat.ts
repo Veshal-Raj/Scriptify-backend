8d655bff3a64491c16284ae4fc9a234f7e15b1a3
Veshal Raj <veshalraj1307@gmail.com>
2024-04-12 17:32:42 +0530

    update: added and made more changes in socket

--- src/infrastructureLayer/services/Chat.ts
+++ src/infrastructureLayer/services/Chat.ts
@@ -2,7 +2,12 @@ import http from 'http'
 import { Server } from 'socket.io'
 import { app } from '../webserver/config/app';
 
-
+interface IUsers {
+    userId: string;
+    socketId: string;
+    lastSeen?: Date;
+    online?: boolean;
+}
 
 export const server = http.createServer(app)
 
@@ -14,20 +19,60 @@ export const io = new Server(server, {
     }
 });
 
+let users: IUsers[] = []
+
+
+const addUser = (userId: string, socketId: string) => {
+    const existingUserIndex = users.findIndex(user => user.userId === userId);
+    if (existingUserIndex !== -1) {
+        const existingUser = users[existingUserIndex]
+        console.log(existingUser)
+        existingUser.socketId = socketId
+    } else {
+        users.push({ userId, socketId})
+    }
+}
+
+const userExists = (userId: string) => {
+    return users.some(user => user.userId === userId);
+};
 
 // Socket.IO connection event
 io.on("connection", (socket) => {
-    console.log('socket connect successfully!!')
-    console.log('User Connected: ', socket.id)
-
-    socket.on("setup", (userData) => {
-        console.log(userData)
-        socket.join(userData._id)
+    
+    socket.on("setup", (userId) => {
+        console.log('userData --- ',userId)
+        addUser(userId, socket.id)
+        
+        socket.join(userId)
         socket.emit("connected")
+        console.log('connected')
+        console.log(users)
     })
 
     socket.on("join chat", (room) => {
+        console.log('room --- ', room)
         socket.join(room);
         console.log("user joined room "+ room)
     })
+
+    socket.on("send_message", (data) => {
+        console.log('message --->> ', data)
+        console.log('receiver id -- ',data.receiver.id)
+        if (userExists(data.receiver.id)) {
+            const receiver = users.find(user => user.userId === data.receiver.id);
+            if (receiver) {
+                console.log('Receiver data:', receiver);    
+                io.to(receiver.socketId).emit("receive_message", data);
+            }
+        } else console.log('no user ')
+        
+        
+    })
+
+    
+
+    socket.on("error", (err) => {
+        console.error('Socket error:', err);
+    });
 });
