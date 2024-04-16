15843925b03da4cdb3fbdf5727129f27add50855
Veshal Raj <veshalraj1307@gmail.com>
2024-04-16 14:39:33 +0530

    feat: added chat user search repository engine added

---
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/chatUserSearch.ts
@@ -0,0 +1,33 @@
+import Message from "../../../models/messageModel";
+import UserModel from "../../../models/userModel";
+
+
+export const chatUserSearch = async (
+    query: string
+) => {
+    const users = await UserModel.find({ "personal_info.username": { $regex: new RegExp(query, "i") } });
+
+    const userList = [];
+
+    // Loop through each user
+    for (const user of users) {
+        // Fetch the latest message for the user (if available)
+        const latestMessage = await Message.findOne({ sender: user._id }).sort({ createdAt: -1 });
+
+        // Construct user object with required information
+        const userObj = {
+            userId: user._id,
+            username: user.personal_info.username,
+            profileImage: user.personal_info.profile_img,
+            lastMessage: latestMessage ? latestMessage.content : '', // Get content of latest message or empty string
+            time: latestMessage ? latestMessage.createdAt : null, // Get timestamp of latest message or null
+            online: true, // Assuming isOnline logic is implemented elsewhere
+        };
+
+        // Add user object to the search results
+        userList.push(userObj);
+
+    }
+    return userList;
+    // return users;
+}
