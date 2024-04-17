237fcecd62de9b4412cea7f954305e29730723d3
Veshal Raj <veshalraj1307@gmail.com>
2024-04-17 21:31:12 +0530

    feat: forgot password user otp added

---
+++ src/usecasesLayer/useCases/user/forgotPasswordUserOtp.ts
@@ -0,0 +1,25 @@
+
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+
+export const forgotPasswordUserOtp = async (
+    otp: string,
+    email: string,
+    next: Next,
+    userRepository: IUserRepository,
+    logger: ILogger
+) => {
+    try {
+        
+        const response = await userRepository.forgotPasswordUserOtp(otp, email)
+        return response
+        
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+}
