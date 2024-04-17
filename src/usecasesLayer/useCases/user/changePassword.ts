1d4c86c6b150c971f2e526f8ce86541711dcf672
Veshal Raj <veshalraj1307@gmail.com>
2024-04-17 21:28:13 +0530

    feat: added changepassword

---
+++ src/usecasesLayer/useCases/user/changePassword.ts
@@ -0,0 +1,24 @@
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+import { IHashpassword } from "../../interface/services/IhashPassword";
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+
+export const changePassword = async(
+    userId: string,
+    newPassword: string,
+    next: Next,
+    bcrypt: IHashpassword,
+    userRepository: IUserRepository,
+    logger: ILogger
+) => {
+    try {
+        newPassword = await bcrypt.createHash(newPassword as string)
+        const response = await userRepository.changePassword(userId, newPassword)
+        return response
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+}
