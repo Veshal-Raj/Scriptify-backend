6ca3f8f7a1f7a4cb6605171eb67768515e088e04
Veshal Raj <veshalraj1307@gmail.com>
2024-04-17 21:28:37 +0530

    feat: added changepassowordnotloggedin

---
+++ src/usecasesLayer/useCases/user/changePasswordNotLoggedIn.ts
@@ -0,0 +1,24 @@
+import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IUserRepository } from "../../interface/repository/IuserRepository";
+import { ILogger } from "../../interface/services/IerrorLog";
+import { IHashpassword } from "../../interface/services/IhashPassword";
+import { ErrorHandler } from "../../middlewares/errorHandler";
+
+
+
+export const changePasswordNotLoggedIn = async(
+    email: string,
+    newPassword: string,
+    next: Next,
+    bcrypt: IHashpassword,
+    userRepository: IUserRepository,
+    logger: ILogger
+) => {
+    try {
+        newPassword = await bcrypt.createHash(newPassword as string)
+        const response = await userRepository.changePasswordNotLoggedIn(email, newPassword)
+        return response
+    } catch (error: unknown | never) {
+        return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
+    }
+}
