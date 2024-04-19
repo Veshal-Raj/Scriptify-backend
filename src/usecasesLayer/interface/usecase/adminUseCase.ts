bdd99513408a893e47edc8394b66593e04ec7582
Veshal Raj <veshalraj1307@gmail.com>
2024-04-19 23:21:00 +0530

    feat: added getAllBlogs, changeBlogStatus, getAllReports

--- src/usecasesLayer/interface/usecase/adminUseCase.ts
+++ src/usecasesLayer/interface/usecase/adminUseCase.ts
@@ -6,4 +6,7 @@ import { IUserResponse } from "../request_response/user";
 export interface IAdminUseCase {
     getAllUser(next: Next): Promise<IUser[] | void>
     changeUserStatus(req: Req, next: Next): Promise<IUserResponse | void>
+    getAllBlogs(next: Next): Promise<any>
+    changeBlogStatus(blogId: string, next: Next): Promise<any>
+    getAllReports(next: Next): Promise<any>
 }
