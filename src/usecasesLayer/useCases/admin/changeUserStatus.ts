decbf270021794eb03c33500203e771ce4d60580
Veshal Raj <veshalraj1307@gmail.com>
2024-04-19 23:22:11 +0530

    update: updated changeUserStatus

--- src/usecasesLayer/useCases/admin/changeUserStatus.ts
+++ src/usecasesLayer/useCases/admin/changeUserStatus.ts
@@ -1,17 +1,18 @@
 import { Next, Ilogger, Req } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IAdminRepository } from "../../interface/repository/IadminRepository";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
 import { IUserResponse } from "../../interface/request_response/user";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
 export const changeUserStatus = async (
-    userRepository: IUserRepository,
+    adminRepository: IAdminRepository,
     req: Req,
     next: Next,
     logger: Ilogger
 ) : Promise<IUserResponse | void> => {
    try {
     console.log(req.params.id , 'in usercases/admin/changeuserstatus')
-        return await userRepository.changeUserStatus(req.params.id as string)
+        return await adminRepository.changeUserStatus(req.params.id as string)
    } catch (error) {
         return next(new ErrorHandler(500, error instanceof Error ? error.message : "Unknown error", logger))
    }
