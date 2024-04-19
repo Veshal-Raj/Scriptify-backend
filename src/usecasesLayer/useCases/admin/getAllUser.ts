8d79bac607e0fe7f5c4a57ac6d85b1d0cd594e1b
Veshal Raj <veshalraj1307@gmail.com>
2024-04-19 23:23:09 +0530

    update: updated getAllUsers

--- src/usecasesLayer/useCases/admin/getAllUser.ts
+++ src/usecasesLayer/useCases/admin/getAllUser.ts
@@ -1,10 +1,11 @@
 import { Next, Ilogger } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { IAdminRepository } from "../../interface/repository/IadminRepository";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
-export const getAllUser = async (userRepository: IUserRepository, next: Next, logger: Ilogger) => {
+export const getAllUser = async (adminRepository: IAdminRepository, next: Next, logger: Ilogger) => {
     try {
-        return await userRepository.getAllUser('user')
+        return await adminRepository.getAllUser('user')
     } catch (error) {
         return next(new ErrorHandler(500, error instanceof Error ? error.message : "Unknown error", logger))
     }
