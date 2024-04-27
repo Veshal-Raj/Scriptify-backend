cc2c248ba4976b920f18c6a41232c6d5ff2b9205
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:10:40 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/unLikeBlogByUser.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/unLikeBlogByUser.ts
@@ -1,31 +1,42 @@
-
 import BlogModel from "../../../models/blogModel";
 import UserModel from "../../../models/userModel";
 
 export const unLikeBlogByUser = async (
-    blog_id: string,
-    userId: string,
-    userModel: typeof UserModel,
-    blogModel: typeof BlogModel
+  blog_id: string,
+  userId: string,
+  userModel: typeof UserModel,
+  blogModel: typeof BlogModel
 ): Promise<any | void> => {
-    try {
-        const user = await userModel.findById(userId);
-        if (!user?.userInteractions.userLikedBlogs.map(blog => String(blog.blogId)).includes(blog_id)) {
-            console.log('User has not liked the blog.');
-            return { success: false, message: 'User has not liked the blog' };
-        }
-
-        // If the user has not already liked the blog, proceed to like it
-        // const updatedBlog = await blogModel.findByIdAndUpdate(blog_id, { $inc: { 'activity.total_likes': 1 } });
-        console.log('bloooooggggid',blog_id)
-        // const id = updatedBlog?._id
-        const updatedUser = await userModel.findByIdAndUpdate(userId, { $push: { 'userInteractions.userLikedBlogs': { blogId: blog_id.toString(), interactionAt: new Date() } } });
-        await userModel.findByIdAndUpdate(userId, { $pull: { 'userInteractions.userLikedBlogs': { blogId: blog_id.toString() } } });
-        // console.log(updatedBlog, updatedUser.userInteractions.userLikedBlogs);
-        const updatedBlog = await blogModel.findByIdAndUpdate(blog_id, { $inc: { 'activity.total_likes': -1 } });
-        return { success: true, message: 'Unlike successful' };
-
-    } catch (error) {
-        throw error
+  try {
+    const user = await userModel.findById(userId);
+    if (
+      !user?.userInteractions.userLikedBlogs
+        .map((blog) => String(blog.blogId))
+        .includes(blog_id)
+    ) {
+      console.log("User has not liked the blog.");
+      return { success: false, message: "User has not liked the blog" };
     }
-}
+
+    const updatedUser = await userModel.findByIdAndUpdate(userId, {
+      $push: {
+        "userInteractions.userLikedBlogs": {
+          blogId: blog_id.toString(),
+          interactionAt: new Date(),
+        },
+      },
+    });
+    await userModel.findByIdAndUpdate(userId, {
+      $pull: {
+        "userInteractions.userLikedBlogs": { blogId: blog_id.toString() },
+      },
+    });
+
+    const updatedBlog = await blogModel.findByIdAndUpdate(blog_id, {
+      $inc: { "activity.total_likes": -1 },
+    });
+    return { success: true, message: "Unlike successful" };
+  } catch (error) {
+    throw error;
+  }
+};
