d03d3ecf47a819422e4d81e3886397e48b9d7ada
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:10:21 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/trendingBlog.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/trendingBlog.ts
@@ -1,25 +1,24 @@
-
 import BlogModel from "../../../models/blogModel";
 
+export const trendingBlog = async (blogModels: typeof BlogModel) => {
+  try {
+    const blogs = await blogModels
+      .find({ draft: false, isBlocked: false })
+      .populate(
+        "author",
+        "personal_info.profile_img personal_info.username -_id"
+      )
+      .sort({
+        "activity.total_reads": -1,
+        "activity.total_likes": -1,
+        publishedAt: -1,
+      })
+      .select("blog_id title publishedAt -_id")
+      .limit(5);
 
-
-export const trendingBlog = async(
-    blogModels: typeof BlogModel
-) => {
-    try {
-        console.log('reached inside the repository/userRepository/user/trendingBlog');
-        
-        const blogs = await blogModels.find({ draft: false, isBlocked: false })
-            .populate("author", "personal_info.profile_img personal_info.username -_id")
-            .sort({ "activity.total_reads": -1, "activity.total_likes": -1, "publishedAt": -1 })
-            .select("blog_id title publishedAt -_id")
-            // .select("blog_id title des banner activity tags publishedAt -_id")
-
-            .limit(5);
-        console.log('trending blogs in userRepository -->> ',blogs)
-        return blogs;
-    } catch (error) {
-        console.error(error);
-        throw error
-    }
-}
+    return blogs;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
