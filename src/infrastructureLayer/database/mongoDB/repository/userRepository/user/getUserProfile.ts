d328ebc6baecddb758757ebd6fd4b5d5b339bcb4
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:05:06 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/getUserProfile.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/getUserProfile.ts
@@ -1,19 +1,16 @@
-import { Next } from "../../../../../types/serverPackageTypes";
 import UserModel from "../../../models/userModel";
 
 export const getUserProfile = async (
-    userId: string,
-    userModels: typeof UserModel,
-    next: Next
+  userId: string,
+  userModels: typeof UserModel
 ): Promise<any | void> => {
-    try {
-        console.log(userId)
-        const result = await userModels.findById( userId)
-         .select("-personal_info.password -google_auth -updatedAt -blogs ")
+  try {
+    const result = await userModels
+      .findById(userId)
+      .select("-personal_info.password -google_auth -updatedAt -blogs ");
 
-         console.log('result -->> ',result)
-        return result;
-    } catch (error) {
-        throw error
-    }
-}
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
