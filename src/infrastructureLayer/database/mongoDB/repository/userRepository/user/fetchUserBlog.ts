e4c1d017661140fb3f459db237ebb83692e44e2c
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:03:27 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/fetchUserBlog.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/fetchUserBlog.ts
@@ -1,27 +1,19 @@
-
 import UserModel from "../../../models/userModel";
 
-
-
-export const fetchUserBlog = async(
-    userId:string,
-    userModel: typeof UserModel,
+export const fetchUserBlog = async (
+  userId: string,
+  userModel: typeof UserModel
 ) => {
-    try {
-        console.log('reached inside the repository/userRepository/user/searchbyquery');
-        console.log('userId --> ', userId)
-        
-        const user = await userModel.findById(userId).populate('blogs')
-        .sort({ "publishedAt": -1 })
-            .select("blog_id title des banner activity tags publishedAt -_id");
+  try {
+    const user = await userModel
+      .findById(userId)
+      .populate("blogs")
+      .sort({ publishedAt: -1 })
+      .select("blog_id title des banner activity tags publishedAt -_id");
 
-        // Access the user's populated blogs
-        // const blogs = user?.blogs;
-
-        console.log('blogs -->>', user);
-        return user;
-    } catch (error) {
-        console.error(error);
-        throw error
-    }
-}
+    return user;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
