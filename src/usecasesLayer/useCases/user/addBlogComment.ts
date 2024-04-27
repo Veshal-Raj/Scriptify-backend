00be15833f865cbe5f98bc933b1c8259c6ff2599
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:16:00 +0530

    update: fixed bug

--- src/usecasesLayer/useCases/user/addBlogComment.ts
+++ src/usecasesLayer/useCases/user/addBlogComment.ts
@@ -1,9 +1,8 @@
 
 
 import { Comment, CommentData } from "../../../@types/general/Comments";
-import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { Ilogger, Next } from "../../../infrastructureLayer/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
-import { ILogger } from "../../interface/services/IerrorLog";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
 
@@ -12,11 +11,12 @@ export const addBlogComment = async (
     comment: Comment,
     next: Next,
     userRepository: IUserRepository,
-    logger: ILogger
+    logger: Ilogger
 ) => {
     try {
        console.log('reached inside the latestBlog usecase engine ')
-       const response = await userRepository.addComment( commentData, comment, next)
+       
+       const response = await userRepository.addComment( commentData, comment[0])
        return response
     } catch (error: unknown | never) {
         return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
