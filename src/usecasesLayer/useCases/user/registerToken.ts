0572a051854c063c7d749e0cda0b1ef544301e2f
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:27:05 +0530

    update: fixed bug

--- src/usecasesLayer/useCases/user/registerToken.ts
+++ src/usecasesLayer/useCases/user/registerToken.ts
@@ -1,6 +1,5 @@
-import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { Ilogger, Next } from "../../../infrastructureLayer/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
-import { ILogger } from "../../interface/services/IerrorLog";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
 
@@ -9,7 +8,7 @@ export const registerToken = async (
     userId: string,
     userRepository: IUserRepository,
     next: Next,
-    logger: ILogger
+    logger: Ilogger
 ) => {
     try {
         const response = await userRepository.registerUserToken(token, userId)
