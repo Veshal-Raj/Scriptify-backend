1eba1dc53c722fa3352bb890e8bd910666836597
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:03:19 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/fetchSimilarBlog.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/fetchSimilarBlog.ts
@@ -1,40 +1,32 @@
-import UserModel from "../../../models/userModel";
 import BlogModel from "../../../models/blogModel";
 
-
-
-export const fetchSimilarBlog = async(
-    tags: string[],
-    blogModels: typeof BlogModel, 
+export const fetchSimilarBlog = async (
+  tags: string[],
+  blogModels: typeof BlogModel
 ) => {
-    try {
-        console.log('reached inside the repository/userRepository/user/latestBlogs');
-        // Create a new instance of BlogModel
-       console.log('tags from the userRepository --> ',tags)
-       const similarBlogs = await blogModels.aggregate([
-        {
-            $match: {
-                tags: { $in: tags } // Find blogs where tags array contains at least one tag from the given array
-            }
+  try {
+    const similarBlogs = await blogModels.aggregate([
+      {
+        $match: {
+          tags: { $in: tags },
         },
-        {
-            $project: {
-                _id: 1,
-                blog_id: 1,
-                title: 1,
-                banner: 1,
-            }
+      },
+      {
+        $project: {
+          _id: 1,
+          blog_id: 1,
+          title: 1,
+          banner: 1,
         },
-        {
-            $limit: 6
-        }
+      },
+      {
+        $limit: 6,
+      },
     ]);
 
-    console.log('similar blogs --> ',similarBlogs)
-    return similarBlogs
-       
-    } catch (error) {
-        console.error(error);
-        throw error
-    }
-}
+    return similarBlogs;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
