4266703166a806cb4f069b97b90028c59d929fa5
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:01:45 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/checkUserSubscribed.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/checkUserSubscribed.ts
@@ -1,21 +1,18 @@
 import UserModel from "../../../models/userModel";
 
+export const checkUserSubscribed = async (userId: string) => {
+  try {
+    const user = await UserModel.findById(userId);
 
-export const checkUserSubscribed = async (
-    userId: string, 
-) => {
-    try {
-        const user = await UserModel.findById(userId);
-
-        if (!user) {
-            throw new Error("User not found");
-        }
-
-        // Check if the user is subscribed
-        const isSubscribed = user.isSubscribed;
-
-        return isSubscribed;
-    } catch (error) {
-        throw error
+    if (!user) {
+      throw new Error("User not found");
     }
-}
+
+    // Check if the user is subscribed
+    const isSubscribed = user.isSubscribed;
+
+    return isSubscribed;
+  } catch (error) {
+    throw error;
+  }
+};
