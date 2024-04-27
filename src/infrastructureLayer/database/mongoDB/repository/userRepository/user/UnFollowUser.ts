43ee84e392c62b54597f28e3255cc3fa24f50ff3
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:10:30 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/UnFollowUser.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/UnFollowUser.ts
@@ -1,51 +1,43 @@
-
 import UserModel from "../../../models/userModel";
 
 export const UnFollowUser = async (
-    authorId: string,
-    userId: string,
-    userModels: typeof UserModel,
+  authorId: string,
+  userId: string,
+  userModels: typeof UserModel
 ): Promise<any | void> => {
-    try {
-        console.log(userId)
-        console.log('autorid', authorId)
-        const user = await userModels.findById( userId)
-        const author = await userModels.findById(authorId)
-        console.log(author)
-        if (!user) {
-            throw new Error("User not found");
-        }
+  try {
+    const user = await userModels.findById(userId);
+    const author = await userModels.findById(authorId);
 
-        if (!author) {
-            throw new Error("Author not found");
-        }
-
-        if (!user.following.includes(authorId)) {
-            // If not, add it to the following list
-            user.following = user.following.filter(id => id !== authorId);
-            // Save the updated user document
-            await user.save();
-            console.log("Successfully unfollowed the author.");
-        } else {
-            console.log("User is not following the author.");
-            return { success: false, message: 'Not following' };
-        }
-
-        if (!author.followers.includes(userId)) {
-            author.followers = author.followers.filter(id => id !== userId);
-            await author.save()
-        } else {
-            console.log('User is not a follower of the author.');
-            return { success: false, message: 'Not a follower' };
-        
-        }
-
-        
-        
-        console.log('result -->> ',user)
-        console.log('<<,,, author --- ',author)
-        return { success: true, message: 'success' };
-    } catch (error) {
-        throw error
+    if (!user) {
+      throw new Error("User not found");
     }
-}
+
+    if (!author) {
+      throw new Error("Author not found");
+    }
+
+    if (!user.following.includes(authorId)) {
+      // If not, add it to the following list
+      user.following = user.following.filter((id) => id !== authorId);
+      // Save the updated user document
+      await user.save();
+      console.log("Successfully unfollowed the author.");
+    } else {
+      console.log("User is not following the author.");
+      return { success: false, message: "Not following" };
+    }
+
+    if (!author.followers.includes(userId)) {
+      author.followers = author.followers.filter((id) => id !== userId);
+      await author.save();
+    } else {
+      console.log("User is not a follower of the author.");
+      return { success: false, message: "Not a follower" };
+    }
+
+    return { success: true, message: "success" };
+  } catch (error) {
+    throw error;
+  }
+};
