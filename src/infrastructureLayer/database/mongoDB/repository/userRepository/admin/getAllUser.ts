6e3f6bc6817688acfe81a219dbee7c152b41676c
Veshal Raj <veshalraj1307@gmail.com>
2024-04-27 23:56:09 +0530

    refactor: formatted the code

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/admin/getAllUser.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/admin/getAllUser.ts
@@ -1,11 +1,10 @@
 import IUser from "../../../../../../entitiesLayer/user";
 import UserModel from "../../../models/userModel";
 
-
 export const getAllUser = async (role: string): Promise<IUser[]> => {
-    try {
-        return await UserModel.find({role: {$eq: role}})
-    } catch (error) {
-        throw error
-    }
-}
+  try {
+    return await UserModel.find({ role: { $eq: role } });
+  } catch (error) {
+    throw error;
+  }
+};
