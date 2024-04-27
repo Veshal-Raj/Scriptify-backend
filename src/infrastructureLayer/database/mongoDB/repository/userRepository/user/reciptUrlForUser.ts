571bd595524e46cdfee00dc5168be1800837b53a
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:08:19 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/reciptUrlForUser.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/reciptUrlForUser.ts
@@ -1,23 +1,19 @@
 import PaymentModel from "../../../models/paymentMode";
 
+export const reciptUrlForUser = async (userId: string) => {
+  try {
+    // Find the payment document for the given user ID
+    const payment = await PaymentModel.findOne({ userId });
 
-
-export const reciptUrlForUser = async (
-    userId: string
-) => {
-    try {
-        // Find the payment document for the given user ID
-        const payment = await PaymentModel.findOne({ userId });
-
-        // If payment document is found, return the receipt URL
-        if (payment) {
-            return payment.receipt_url;
-        } else {
-            // Payment not found for the user ID
-            return null;
-        }
-    } catch (error) {
-        console.error('Error fetching receipt URL:', error);
-        return null;
+    // If payment document is found, return the receipt URL
+    if (payment) {
+      return payment.receipt_url;
+    } else {
+      // Payment not found for the user ID
+      return null;
     }
-}
+  } catch (error) {
+    console.error("Error fetching receipt URL:", error);
+    return null;
+  }
+};
