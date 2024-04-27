a06887e8305a6fb858837876138dd78325f213e4
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:08:35 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/replyComment.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/replyComment.ts
@@ -1,55 +1,36 @@
-
-import { Comment, CommentData } from "../../../../../../@types/general/Comments";
-import BlogModel from "../../../models/blogModel";
+import { CommentData } from "../../../../../../@types/general/Comments";
 import CommentModel from "../../../models/commentModel";
-import UserModel from "../../../models/userModel";
 
 export const replyComment = async (
-    comment: string,
-    parentCommentId: string,
-    commentData: CommentData,
-    userModel: typeof UserModel,
-    blogModel: typeof BlogModel,
-    
+  comment: string,
+  parentCommentId: string,
+  commentData: CommentData
 ): Promise<any | void> => {
-    try {
-        console.log('comment >>>', comment)
-        console.log('parent Id >>>', parentCommentId)
-        const isParentComment = await CommentModel.findById(parentCommentId);
+  try {
+    const isParentComment = await CommentModel.findById(parentCommentId);
 
-        if (!isParentComment) {
-            throw new Error('Parent comment not found');
-        }
-
-        console.log('parent comment ', isParentComment)
-        
-        const { userId, authorId, blogId, _id} = commentData
-
-        const newComment = new CommentModel({
-            blog_id: _id, // Assuming _id is the ID of the blog
-            blog_author: authorId,
-            comment: comment,
-            commented_by: userId,
-            isReply: true
-        });
-
-        const commentId = newComment._id
-        const replyComment = await newComment.save();
-        console.log('saved comment >> ', replyComment )
-
-        const replyCommentId = replyComment._id
-        await CommentModel.updateOne(
-            { _id: parentCommentId }, // Find the parent comment by its ID
-            { $push: { children: replyCommentId } } // Push the ID of the saved reply comment to the children array
-
-            );
-            
-            console.log(isParentComment)
-        
-
-      
-
-    } catch (error) {
-        throw error
+    if (!isParentComment) {
+      throw new Error("Parent comment not found");
     }
-}
+
+    const { userId, authorId, _id } = commentData;
+
+    const newComment = new CommentModel({
+      blog_id: _id, // Assuming _id is the ID of the blog
+      blog_author: authorId,
+      comment: comment,
+      commented_by: userId,
+      isReply: true,
+    });
+
+    const replyComment = await newComment.save();
+
+    const replyCommentId = replyComment._id;
+    await CommentModel.updateOne(
+      { _id: parentCommentId }, // Find the parent comment by its ID
+      { $push: { children: replyCommentId } } // Push the ID of the saved reply comment to the children array
+    );
+  } catch (error) {
+    throw error;
+  }
+};
