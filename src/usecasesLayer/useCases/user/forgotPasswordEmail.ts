67c44276b3c27a4211fa2f5df9a8a19489ded1e2
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:23:53 +0530

    update: fixed bug

--- src/usecasesLayer/useCases/user/forgotPasswordEmail.ts
+++ src/usecasesLayer/useCases/user/forgotPasswordEmail.ts
@@ -1,7 +1,6 @@
-import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { Ilogger, Next } from "../../../infrastructureLayer/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
 import { IcreateOTP } from "../../interface/services/IcreateOTP";
-import { ILogger } from "../../interface/services/IerrorLog";
 import { ISendMail } from "../../interface/services/IsendMail";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
@@ -14,7 +13,7 @@ export const forgotPasswordEmail = async (
     userrepository: IUserRepository,
     otpGenerator: IcreateOTP,
     sendMail: ISendMail,
-    logger: ILogger
+    logger: Ilogger
 ) => {
     try {
         const response = await userrepository.forgotPasswordEmail(email, otpGenerator, sendMail)
