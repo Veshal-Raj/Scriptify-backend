b33b8db0a745f1643eb619127e1e8123014e9617
Veshal Raj <veshalraj1307@gmail.com>
2024-04-27 23:53:57 +0530

    refactor: refactored the code

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/admin/changeBlogStatus.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/admin/changeBlogStatus.ts
@@ -1,32 +1,22 @@
 import BlogModel from "../../../models/blogModel";
 
+export const changeBlogStatus = async (blogId: string) => {
+  try {
+    const blog = await BlogModel.findById(blogId);
 
+    // If the blog is found
+    if (blog) {
+      // Reverse the value of isBlocked
+      const updatedStatus = !blog.isBlocked;
 
-export const changeBlogStatus = async (
-    blogId: string
-) => {
-    try {
-        console.log('blog id --- ', blogId)
-        // const blog_id = blogId?.blogId
-        // Find the blog by ID
-        const blog = await BlogModel.findById(blogId);
-        console.log('blog --- ', blog)
+      // Update the blog's isBlocked status
+      await BlogModel.findByIdAndUpdate(blogId, { isBlocked: updatedStatus });
 
-        // If the blog is found
-        if (blog) {
-            // Reverse the value of isBlocked
-            const updatedStatus = !blog.isBlocked;
-
-            // Update the blog's isBlocked status
-            const a = await BlogModel.findByIdAndUpdate(blogId, { isBlocked: updatedStatus });
-
-            console.log('a -- ', a)
-            // Return the updated blog
-            return updatedStatus;
-        } else {
-            throw new Error('Blog not found');
-        }
-    } catch (error) {
-        throw error;
+      return updatedStatus;
+    } else {
+      throw new Error("Blog not found");
     }
-}
+  } catch (error) {
+    throw error;
+  }
+};
