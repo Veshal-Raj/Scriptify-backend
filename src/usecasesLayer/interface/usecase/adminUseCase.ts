697c70d3c9a840f5da802126b4b9ea88b4155aac
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:12:59 +0530

    refactor: removed unwanted code and formatted

--- src/usecasesLayer/interface/usecase/adminUseCase.ts
+++ src/usecasesLayer/interface/usecase/adminUseCase.ts
@@ -5,7 +5,7 @@ import { IUserResponse } from "../request_response/user";
 
 export interface IAdminUseCase {
     getAllUser(next: Next): Promise<IUser[] | void>
-    changeUserStatus(req: Req, next: Next): Promise<IUserResponse | void>
+    changeUserStatus(userId: string, next: Next): Promise<IUserResponse | void | IUser | null>
     getAllBlogs(next: Next): Promise<any>
     changeBlogStatus(blogId: string, next: Next): Promise<any>
     getAllReports(next: Next): Promise<any>
