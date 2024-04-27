4fd9e8b877e2842deee19d43fa27dfa33648a4af
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:09:22 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/resendOtp.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/resendOtp.ts
@@ -1,45 +1,31 @@
-import { IcreateOTP } from "../../../../../../usecasesLayer/interface/services/IcreateOTP"
-import { ISendMail } from "../../../../../../usecasesLayer/interface/services/IsendMail"
+import { IcreateOTP } from "../../../../../../usecasesLayer/interface/services/IcreateOTP";
+import { ISendMail } from "../../../../../../usecasesLayer/interface/services/IsendMail";
 import OtpModel from "../../../models/otpModel";
-import UserModel from "../../../models/userModel";
-
-
 
 export const resendOtp = async (
-    email: string,
-    sendMail: ISendMail,
-    otpGenerator: IcreateOTP
+  email: string,
+  sendMail: ISendMail,
+  otpGenerator: IcreateOTP
 ) => {
-    try {
-        console.log('email ----- ', email)
-        // const existingUser = await UserModel.findOne({ 'personal_info.email': email });
-        // console.log('existing user --- ', existingUser)
-        
-        const existingOTP = await OtpModel.findOne({ email });
-        console.log('existingOTP --- ', existingOTP)
+  try {
+    const existingOTP = await OtpModel.findOne({ email });
 
-    if (existingOTP) return { message: 'Otp already send!'}
+    if (existingOTP) return { message: "Otp already send!" };
 
-    if ( !existingOTP) {
+    if (!existingOTP) {
+      const otp = await otpGenerator.generateOTP();
 
-        const otp = await otpGenerator.generateOTP() 
-        console.log('otp --- ', otp)
-        console.log('email --- ',email)
-        const mail = await sendMail.sendEmailVerification('User',email, otp)
-        console.log('mail --- ', mail)
-        
-        const otpDoc =   await OtpModel.create({ email, otp });
-        console.log(otpDoc)
+      const mail = await sendMail.sendEmailVerification("User", email, otp);
 
-      console.log('Email already exists:', email);
-      return { message: "OTP send successfully"}
+      const otpDoc = await OtpModel.create({ email, otp });
+
+      return { message: "OTP send successfully" };
     } else {
       // Email doesn't exist, handle accordingly (e.g., send error message)
-      console.log('Email does not exist:', email);
-      return { message: 'Email does not exist'}
+      console.log("Email does not exist:", email);
+      return { message: "Email does not exist" };
     }
-        
-    } catch (error) {
-        throw error
-    }
-}
+  } catch (error) {
+    throw error;
+  }
+};
