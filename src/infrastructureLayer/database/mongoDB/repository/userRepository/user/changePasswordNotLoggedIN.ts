b6d89716b28111a837978b354099916468a1c813
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:01:09 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/changePasswordNotLoggedIN.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/changePasswordNotLoggedIN.ts
@@ -1,24 +1,20 @@
-import UserModel from "../../../models/userModel"
+import UserModel from "../../../models/userModel";
 
-
-
-export const changePasswordNotLoggedIN = async(
-    email: string,
-    newPassword: string
+export const changePasswordNotLoggedIN = async (
+  email: string,
+  newPassword: string
 ) => {
-    try {
-        console.log('email -- ', email, 'new password --- ', newPassword)
-        const user = await UserModel.findOne({ "personal_info.email": email });
-        if (!user) {
-            throw new Error('User not found')
-        }
-        console.log('user data --- ', user)
-        console.log('newpassword -- ', newPassword)
-        user.personal_info.password = newPassword
-        await user.save()
-
-        return user
-    } catch (error) {
-        throw error
+  try {
+    const user = await UserModel.findOne({ "personal_info.email": email });
+    if (!user) {
+      throw new Error("User not found");
     }
-} 
+
+    user.personal_info.password = newPassword;
+    await user.save();
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
