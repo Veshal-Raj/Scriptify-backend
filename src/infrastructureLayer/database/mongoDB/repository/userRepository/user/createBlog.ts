962345e1d6d90f68270a853cc191d75c881a1523
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:02:06 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/createBlog.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/createBlog.ts
@@ -1,44 +1,46 @@
 import UserModel from "../../../models/userModel";
 import BlogModel from "../../../models/blogModel";
 
-
-
-export const CreateBlog = async(
-    title: string, des: string, banner: string, content: any, tags: string[], authorId: string, blog_id: string, draft: boolean,
-    userModels: typeof UserModel, blogModels: typeof BlogModel
+export const CreateBlog = async (
+  title: string,
+  des: string,
+  banner: string,
+  content: any,
+  tags: string[],
+  authorId: string,
+  blog_id: string,
+  draft: boolean,
+  userModels: typeof UserModel,
+  blogModels: typeof BlogModel
 ) => {
-   try {
-    console.log('reached inside the repository/userRepository/user/createBlog')
+  try {
     // Create a new instance of BlogModel
     const blog = new blogModels({
-        title,
-        des,
-        banner,
-        content,
-        tags,
-        author: authorId,
-        blog_id,
-        draft: Boolean(draft),
-      });
-      console.log('blog -->> ', blog)
-      // Save the new blog to the database
-      const savedBlog = await blog.save();
-      console.log('saved Blog -->> ', savedBlog)
-  
-      // Update the corresponding user's information
-      const incrementVal = draft ? 0 : 1;
-      await userModels.findOneAndUpdate(
-        { _id: authorId },
-        {
-          $inc: { "account_info.total_posts": incrementVal },
-          $push: { blogs: savedBlog._id },
-        }
-      );
-  
-      // Return the ID of the saved blog
-      return { id: savedBlog.blog_id };
-        
-   } catch (error) {
-        throw error
-   }
-}
+      title,
+      des,
+      banner,
+      content,
+      tags,
+      author: authorId,
+      blog_id,
+      draft: Boolean(draft),
+    });
+    // Save the new blog to the database
+    const savedBlog = await blog.save();
+
+    // Update the corresponding user's information
+    const incrementVal = draft ? 0 : 1;
+    await userModels.findOneAndUpdate(
+      { _id: authorId },
+      {
+        $inc: { "account_info.total_posts": incrementVal },
+        $push: { blogs: savedBlog._id },
+      }
+    );
+
+    // Return the ID of the saved blog
+    return { id: savedBlog.blog_id };
+  } catch (error) {
+    throw error;
+  }
+};
