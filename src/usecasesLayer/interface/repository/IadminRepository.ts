eb294a8e5a09638a579183e15c035faf572e6c27
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:12:31 +0530

    update: made changes

--- src/usecasesLayer/interface/repository/IadminRepository.ts
+++ src/usecasesLayer/interface/repository/IadminRepository.ts
@@ -7,7 +7,7 @@ import { IUserResponse } from "../request_response/user";
 
 export interface IAdminRepository {
     getAllUser(role: string): Promise<IUser []>;
-    changeUserStatus(id: string): Promise<IUserResponse | null | IUser>
+    changeUserStatus(userId: string): Promise<IUserResponse | null | IUser | void>
     getAllBlogs(): Promise<any>
     changeBlogStatus(blogId: string): Promise<any>
     getAllReports(): Promise<any>
