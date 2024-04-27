f337483b19d16aea5bee1f05810172cb988741ad
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:28:46 +0530

    update: fixed bug

--- src/usecasesLayer/useCases/user/unFollowUser.ts
+++ src/usecasesLayer/useCases/user/unFollowUser.ts
@@ -1,6 +1,5 @@
-import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { Ilogger, Next } from "../../../infrastructureLayer/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
-import { ILogger } from "../../interface/services/IerrorLog";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
 
@@ -9,11 +8,11 @@ export const unFollowUser = async (
     userId: string,
     userRepository: IUserRepository,
     next: Next,
-    logger: ILogger
+    logger: Ilogger
 ) => {
     try {
        console.log('reached inside the latestBlog usecase engine ')
-       const response = await userRepository.unfollowUser(authorId,userId, next)
+       const response = await userRepository.unfollowUser(authorId,userId)
        return response
     } catch (error: unknown | never) {
         return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
