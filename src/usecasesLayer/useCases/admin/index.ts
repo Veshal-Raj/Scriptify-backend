1f42f3bfd9161ba2171f8d3822b6487e97425664
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:15:51 +0530

    update: added more imports and exports

--- src/usecasesLayer/useCases/admin/index.ts
+++ src/usecasesLayer/useCases/admin/index.ts
@@ -1,4 +1,7 @@
 import { getAllUser } from "./getAllUser";
 import { changeUserStatus } from "./changeUserStatus";
+import { getAllBlogs } from "./getAllBlogs";
+import { changeBlogStatus } from "./changeBlogStatus";
+import { getAllReports } from "./getAllReports";
 
-export { getAllUser, changeUserStatus }
+export { getAllUser, changeUserStatus, getAllBlogs, changeBlogStatus, getAllReports }
