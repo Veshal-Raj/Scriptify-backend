5158c3a6a0f04a92923427818ad636d244b8c411
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:04:22 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/FollowUser.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/FollowUser.ts
@@ -1,69 +1,57 @@
-
 import NotificationModel from "../../../models/notificationModel";
 import UserModel from "../../../models/userModel";
 import { sendNotification } from "../../../../../services/notification";
 
 export const FollowUser = async (
-    authorId: string,
-    userId: string,
-    userModels: typeof UserModel,
+  authorId: string,
+  userId: string,
+  userModels: typeof UserModel
 ): Promise<any | void> => {
-    try {
-        console.log(userId)
-        console.log('autorid', authorId)
-        const user = await userModels.findById( userId)
-        const author = await userModels.findById(authorId)
-        console.log(author)
-        if (!user) {
-            throw new Error("User not found");
-        }
+  try {
+    const user = await userModels.findById(userId);
+    const author = await userModels.findById(authorId);
 
-        if (!author) {
-            throw new Error("Author not found");
-        }
+    if (!user) throw new Error("User not found");
 
-        if (!user.following.includes(authorId)) {
-            // If not, add it to the following list
-            user.following.push(authorId);
-            // Save the updated user document
-            await user.save();
+    if (!author) throw new Error("Author not found");
 
-            await NotificationModel.create({
-                type: 'follow',
-                notification_for: authorId,
-                user: userId,
-                seen: false
-            })
+    if (!user.following.includes(authorId)) {
+      // If not, add it to the following list
+      user.following.push(authorId);
+      // Save the updated user document
+      await user.save();
 
-            const authorData = await UserModel.findById(authorId);
-            if (!authorData) {
-                throw new Error("Author data not found")
-            }
-            const title = "You have a new follower 👱🏻‍♂️"
-            const body = `New follower: ${user.personal_info.username}`
-            if (authorData.NotificationToken) {
+      await NotificationModel.create({
+        type: "follow",
+        notification_for: authorId,
+        user: userId,
+        seen: false,
+      });
 
-                await sendNotification(authorData.NotificationToken,body , title)
-            }
-        } else {
-            console.log("User is already following the author.");
-            return  { success: false, message: 'Already following'}
-        }
-
-        if (!author.followers.includes(userId)) {
-            author.followers.push(userId)
-            await author.save()
-        } else {
-            console.log('User is already a follower of the author.')
-            return  { success: false, message: 'Already follower'}
-        }
-
-        
-        
-        console.log('result -->> ',user)
-        console.log('<<,,, author --- ',author)
-        return { success: true, message: 'success' };
-    } catch (error) {
-        throw error
+      const authorData = await UserModel.findById(authorId);
+      if (!authorData) {
+        throw new Error("Author data not found");
+      }
+      const title = "You have a new follower 👱🏻‍♂️";
+      const body = `New follower: ${user.personal_info.username}`;
+      if (authorData.NotificationToken) {
+        await sendNotification(authorData.NotificationToken, body, title);
+      }
+    } else {
+      console.log("User is already following the author.");
+      return { success: false, message: "Already following" };
     }
-}
+
+    if (!author.followers.includes(userId)) {
+      author.followers.push(userId);
+      await author.save();
+    } else {
+      console.log("User is already a follower of the author.");
+      return { success: false, message: "Already follower" };
+    }
+
+    return { success: true, message: "success" };
+  } catch (error) {
+    throw error;
+  }
+};
