e7c56f78e0e28906c696538810eec83d55ed50dc
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:04:47 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/forgotPasswordOtp.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/forgotPasswordOtp.ts
@@ -1,23 +1,17 @@
 import OtpModel from "../../../models/otpModel";
 
+export const forgotPasswordOtp = async (otp: string, email: string) => {
+  try {
+    const otpDocument = await OtpModel.findOne({ email, otp });
 
-
-
-export const forgotPasswordOtp = async (
-    otp: string, 
-    email: string
-) => {
-    try {
-        const otpDocument = await OtpModel.findOne({ email, otp });
-
-        if (otpDocument) {
-            // Email and OTP match
-            return { status: true, message: 'Email and OTP match' };
-        } else {
-            // Email and OTP do not match
-            return { status: false, message: 'Email and OTP do not match' };
-        }
-    } catch (error) {
-        throw error
+    if (otpDocument) {
+      // Email and OTP match
+      return { status: true, message: "Email and OTP match" };
+    } else {
+      // Email and OTP do not match
+      return { status: false, message: "Email and OTP do not match" };
     }
-}
+  } catch (error) {
+    throw error;
+  }
+};
