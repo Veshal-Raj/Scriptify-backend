e863abed083963615b69251bd68f331c62af47f1
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:10:10 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/sendChatByUser.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/sendChatByUser.ts
@@ -1,36 +1,31 @@
-import { IConversation } from "../../../../../../@types/general/chatData"
-import ChatModel from "../../../models/chatModel"
-import Message from "../../../models/messageModel"
+import { IConversation } from "../../../../../../@types/general/chatData";
+import ChatModel from "../../../models/chatModel";
+import Message from "../../../models/messageModel";
 
+export const sendChatByUser = async (data: IConversation) => {
+  try {
+    let chat = await ChatModel.findOne({
+      users: { $all: [data.sender.id, data.receiver.id] },
+    });
 
-export const sendChatByUser = async (
-    data: IConversation
-) => {
-    try {
-        // console.log('data ------------- ', data)
-        let chat = await ChatModel.findOne({
-            users: { $all: [data.sender.id, data.receiver.id]}
-        })
-
-        if (!chat) {
-            chat = await ChatModel.create({
-                users: [data.sender.id, data.receiver.id]
-            })
-        }
-
-        const message = await Message.create({
-            sender: data.sender.id,
-            receiver: data.receiver.id,
-            content: data.message,
-            chat:chat._id
-        })
-
-        chat.latestMessage = message._id
-        await chat.save()
-
-        console.log('Chat and message created successfully');
-
-    } catch (error) {
-        throw error
+    if (!chat) {
+      chat = await ChatModel.create({
+        users: [data.sender.id, data.receiver.id],
+      });
     }
-}
+
+    const message = await Message.create({
+      sender: data.sender.id,
+      receiver: data.receiver.id,
+      content: data.message,
+      chat: chat._id,
+    });
+
+    chat.latestMessage = message._id;
+    await chat.save();
+
+    console.log("Chat and message created successfully");
+  } catch (error) {
+    throw error;
+  }
+};
