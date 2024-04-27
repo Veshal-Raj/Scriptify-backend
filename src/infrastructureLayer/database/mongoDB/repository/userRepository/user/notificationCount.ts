b79b7fe15091ca9c31505d1b1adfa4d6ae36edc6
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:07:58 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/notificationCount.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/notificationCount.ts
@@ -1,13 +1,12 @@
 import NotificationModel from "../../../models/notificationModel";
 
-
-
-export const notificationCount = async ( userId: string) => {
-    try {
-        const count = await NotificationModel.countDocuments({ notification_for: userId, seen: false });
-        console.log('notification count ---- ', count)
-        return count
-    } catch (error) {
-        throw error
-    }
-}
+export const notificationCount = async (userId: string) => {
+  try {
+    return await NotificationModel.countDocuments({
+      notification_for: userId,
+      seen: false,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
