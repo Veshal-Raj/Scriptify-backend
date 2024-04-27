f4449f401e602727fe6e847d8a2e4c2ab54bfae7
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:06:14 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/initialBlogComments.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/initialBlogComments.ts
@@ -1,69 +1,54 @@
-
-import { IBlog } from "../../../../../../entitiesLayer/blog";
 import IComment from "../../../../../../entitiesLayer/comments";
 import BlogModel from "../../../models/blogModel";
 
-interface CommentDetails {
-    _id: string;
-    text: string;
-    commenter: {
-        _id: string;
-        name: string;
-    };
-    createdAt: Date;
-}
-
 export const initialBlogComments = async (
-    blogId: string,
-    blogModel: typeof BlogModel,
-   
+  blogId: string,
+  blogModel: typeof BlogModel
 ): Promise<any | void> => {
-    try {
-        const blog= await blogModel.findById(blogId);
+  try {
+    const blog = await blogModel.findById(blogId);
 
-        if (!blog) {
-            throw new Error('Blog not found');
-        }
-
-        // Populate the 'comments' field of the blog with comment details
-        const blogComments =  await blog.populate({
-            path: 'comments',
-            select: 'comment commented_by commentedAt isReply',
-            populate: {
-                path: 'commented_by',
-                select: 'personal_info.username'
-            }
-        });
-
-        // Extract and format the comment details
-        console.log('initial comments --- ',blogComments.comments)
-
-        if (blogComments.comments) {
-
-            const filteredComments: IComment[] = blogComments.comments.filter((comment: IComment) => comment.isReply === false);
-            
-            const formattedComments: any[] = filteredComments.map((comment: IComment) => ({
-                commentID: comment._id,
-                commentedUser: {
-                    id: comment.commented_by._id,
-                    username: comment.commented_by.personal_info.username
-                },
-                comment: comment.comment,
-                commentTime: comment.commentedAt.toISOString() // Convert date to string
-            }));
-            console.log('formattedComments ', formattedComments)
-
-            formattedComments.sort((a, b) => new Date(b.commentTime).getTime() - new Date(a.commentTime).getTime());
-
-            return formattedComments
-        } else {
-            return null
-        }
-        
-        
-       
-        
-    } catch (error) {
-        throw error
+    if (!blog) {
+      throw new Error("Blog not found");
     }
-}
+
+    // Populate the 'comments' field of the blog with comment details
+    const blogComments = await blog.populate({
+      path: "comments",
+      select: "comment commented_by commentedAt isReply",
+      populate: {
+        path: "commented_by",
+        select: "personal_info.username",
+      },
+    });
+
+    if (blogComments.comments) {
+      const filteredComments: IComment[] = blogComments.comments.filter(
+        (comment: IComment) => comment.isReply === false
+      );
+
+      const formattedComments: any[] = filteredComments.map(
+        (comment: IComment) => ({
+          commentID: comment._id,
+          commentedUser: {
+            id: comment.commented_by._id,
+            username: comment.commented_by.personal_info.username,
+          },
+          comment: comment.comment,
+          commentTime: comment.commentedAt.toISOString(), // Convert date to string
+        })
+      );
+
+      formattedComments.sort(
+        (a, b) =>
+          new Date(b.commentTime).getTime() - new Date(a.commentTime).getTime()
+      );
+
+      return formattedComments;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
