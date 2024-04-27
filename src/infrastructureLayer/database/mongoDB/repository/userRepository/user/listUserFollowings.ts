46c8d46032c79bb058ff2c4fa19ac6631689996b
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:07:42 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/listUserFollowings.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/listUserFollowings.ts
@@ -1,26 +1,24 @@
-
 import UserModel from "../../../models/userModel";
 
 export const listUserFollowings = async (
-    userId: string,
-    userModel: typeof UserModel,
-    
+  userId: string,
+  userModel: typeof UserModel
 ): Promise<any | void> => {
-    try {
-        const user = await userModel.findById(userId).populate('following', 'personal_info.profile_img personal_info.username _id');
-        console.log('user ,,,,,,',user)
-        if (!user) {
-            console.log('User not found.');
-            return { success: false, message: 'User not found.' };
-        }
-        
-        console.log(user.followers);
-        console.log(user.following)
+  try {
+    const user = await userModel
+      .findById(userId)
+      .populate(
+        "following",
+        "personal_info.profile_img personal_info.username _id"
+      );
 
-        // Return the followers with populated fields
-        return user.following;
-        
-    } catch (error) {
-        throw error
+    if (!user) {
+      console.log("User not found.");
+      return { success: false, message: "User not found." };
     }
-}
+
+    return user.following;
+  } catch (error) {
+    throw error;
+  }
+};
