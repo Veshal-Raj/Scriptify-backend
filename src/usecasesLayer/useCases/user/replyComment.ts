e350ac09c83c63738e207ef032209c2391d40a12
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:27:15 +0530

    update: fixed bug

--- src/usecasesLayer/useCases/user/replyComment.ts
+++ src/usecasesLayer/useCases/user/replyComment.ts
@@ -1,9 +1,8 @@
 
 
 import { CommentData } from "../../../@types/general/Comments";
-import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { Ilogger, Next } from "../../../infrastructureLayer/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
-import { ILogger } from "../../interface/services/IerrorLog";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
 
@@ -13,11 +12,11 @@ export const replyComment = async (
     commentData: CommentData,
     next: Next,
     userRepository: IUserRepository,
-    logger: ILogger
+    logger: Ilogger
 ) => {
     try {
        console.log('reached inside the latestBlog usecase engine ')
-       const response = await userRepository.addReplyComments(comment, parentCommentId ,commentData, next)
+       const response = await userRepository.addReplyComments(comment, parentCommentId ,commentData)
        return response
     } catch (error: unknown | never) {
         return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
