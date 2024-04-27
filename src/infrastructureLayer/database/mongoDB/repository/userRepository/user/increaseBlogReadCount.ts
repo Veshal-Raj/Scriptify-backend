f59b04178b4a910cc739c053d1e111a3014c65a7
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:05:37 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/increaseBlogReadCount.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/increaseBlogReadCount.ts
@@ -1,49 +1,50 @@
 import UserModel from "../../../models/userModel";
 import BlogModel from "../../../models/blogModel";
 
-
-export const increaseBlogReadCount = async(
-    userId: string,
-    blogId: string,
-    blogModels: typeof BlogModel,
-    userModels: typeof UserModel 
+export const increaseBlogReadCount = async (
+  userId: string,
+  blogId: string,
+  blogModels: typeof BlogModel,
+  userModels: typeof UserModel
 ) => {
-    try {
-       // Check if the user has already read the blog
-       const user = await userModels.findById(userId);
-       
-       if (!user) {
-           throw new Error("User not found");
-       }
-       const blog = await blogModels.findOne({  blog_id: blogId });
+  try {
+    // Check if the user has already read the blog
+    const user = await userModels.findById(userId);
 
-       if (blog) {
-
-           const isBlogAlreadyRead = user.userInteractions.userReadBlogs.some(interaction => interaction.blogId.toString() === blog._id.toString());
-           
-            if (isBlogAlreadyRead) {
-                console.log("Blog already read by the user.");
-                return 'Blog already read by the user';
-            }
-
-            blog.activity.total_reads += 1;
-    
-            await blog.save();
-    
-            const blog_id = blog._id
-
-            user.userInteractions.userReadBlogs.push({ blogId: blog_id, interactionAt: new Date() });
-
-            await user.save();
-
-            return "Blog read count increased successfully."
-       } else {
-            return "Blog  not present"
-       }
-        
-        
-    } catch (error) {
-        console.error(error);
-        throw error
+    if (!user) {
+      throw new Error("User not found");
     }
-}
+    const blog = await blogModels.findOne({ blog_id: blogId });
+
+    if (blog) {
+      const isBlogAlreadyRead = user.userInteractions.userReadBlogs.some(
+        (interaction) => interaction.blogId.toString() === blog._id.toString()
+      );
+
+      if (isBlogAlreadyRead) {
+        console.log("Blog already read by the user.");
+        return "Blog already read by the user";
+      }
+
+      blog.activity.total_reads += 1;
+
+      await blog.save();
+
+      const blog_id = blog._id;
+
+      user.userInteractions.userReadBlogs.push({
+        blogId: blog_id.toString(),
+        interactionAt: new Date(),
+      });
+
+      await user.save();
+
+      return "Blog read count increased successfully.";
+    } else {
+      return "Blog  not present";
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
