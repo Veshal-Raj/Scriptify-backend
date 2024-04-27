55bba36ad50c2019bbd10bf343fbcd4422ed6ea4
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:03:43 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/fetchUserNotification.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/fetchUserNotification.ts
@@ -1,69 +1,72 @@
 import BlogModel from "../../../models/blogModel";
-import NotificationModel from "../../../models/notificationModel"
+import NotificationModel from "../../../models/notificationModel";
 import UserModel from "../../../models/userModel";
 
+export const fetchUserNotification = async (userId: string) => {
+  try {
+    const userNotifications = await NotificationModel.find({
+      notification_for: userId,
+    })
+      .populate("blog") // Populate the 'blog' field with actual blog data
+      .populate("comment"); // Populate the 'comment' field with actual comment data
 
+    const user = await UserModel.findById(userId);
 
-export const fetchUserNotification = async( userId: string ) => {
-    try {
-
-        console.log('userid  --- ', userId)
-        const userNotifications = await NotificationModel.find({ notification_for: userId })
-        .populate('blog') // Populate the 'blog' field with actual blog data
-        .populate('comment'); // Populate the 'comment' field with actual comment data
-
-        console.log('usernotification -- ', userNotifications)
-        const user = await UserModel.findById(userId);    
-        
-        
-        
-        const formattedNotifications = await Promise.all(userNotifications.map(async (notification) => {
+    const formattedNotifications = await Promise.all(
+      userNotifications.map(async (notification) => {
         const followedUser = await UserModel.findById(notification.user);
-        console.log('---- notification ---- ', notification)
-        switch (notification.type as "comment" | "reply" | "message" | "like" | "follow" | "save") {
-            case 'follow':
-                return {
-                    type: notification.type,
-                    username: followedUser?.personal_info.username || 'Unknown',
-                    userImage: followedUser?.personal_info.profile_img || 'Default User Image',
-                    time: notification.createdAt,
-                    userId: notification.user,
-                    notificationId: notification._id,
-                    seen: notification.seen
-                };
-            case 'like':
-            case 'save':
-            case 'comment':
-                const blog = await BlogModel.findById(notification.blog);
-                
-                return {
-                    type: notification.type,
-                    username: followedUser?.personal_info.username || 'Unknown',
-                    blogBannerImage: blog?.banner || 'Default Blog Banner Image',
-                    time: notification.createdAt,
-                    blogId: blog?.blog_id,
-                    notificationId: notification._id,
-                    seen: notification.seen
 
-                };
-            default:
-                return null;
+        switch (
+          notification.type as
+            | "comment"
+            | "reply"
+            | "message"
+            | "like"
+            | "follow"
+            | "save"
+        ) {
+          case "follow":
+            return {
+              type: notification.type,
+              username: followedUser?.personal_info.username || "Unknown",
+              userImage:
+                followedUser?.personal_info.profile_img || "Default User Image",
+              time: notification.createdAt,
+              userId: notification.user,
+              notificationId: notification._id,
+              seen: notification.seen,
+            };
+          case "like":
+          case "save":
+          case "comment":
+            const blog = await BlogModel.findById(notification.blog);
+
+            return {
+              type: notification.type,
+              username: followedUser?.personal_info.username || "Unknown",
+              blogBannerImage: blog?.banner || "Default Blog Banner Image",
+              time: notification.createdAt,
+              blogId: blog?.blog_id,
+              notificationId: notification._id,
+              seen: notification.seen,
+            };
+          default:
+            return null;
         }
-    }));
+      })
+    );
 
     const result = formattedNotifications
-    .filter(notification => notification !== null)
-    .sort((a, b) => {
+      .filter((notification) => notification !== null)
+      .sort((a, b) => {
         if (a && b) {
-            return b.time.getTime() - a.time.getTime();
+          return b.time.getTime() - a.time.getTime();
         }
         return 0;
-    });
+      });
 
-    console.log('formattedNotification -- ', result)
-    if (result) return result
-        // return result;
-    } catch (error) {
-        throw error
-    }
-}
+    if (result) return result;
+  } catch (error) {
+    throw error;
+  }
+};
