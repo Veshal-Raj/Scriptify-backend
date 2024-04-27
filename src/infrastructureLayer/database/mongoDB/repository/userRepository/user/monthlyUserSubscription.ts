adf14c9249e74644aee25eee593a22bb6d1fdde4
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:07:51 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/monthlyUserSubscription.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/monthlyUserSubscription.ts
@@ -1,6 +1,4 @@
 import { IPaymentService } from "../../../../../../usecasesLayer/interface/services/IpaymentService";
-import UserModel from "../../../models/userModel";
-import PaymentModel from "../../../models/paymentMode";
 
 export const monthlyUserSubscription = async (
     userId: string,
@@ -8,23 +6,7 @@ export const monthlyUserSubscription = async (
     paymentService: IPaymentService
 ) => {
     const paymentData = await paymentService.pay(userId,subscriptionType)
-    console.log('payment Data ', paymentData)
-
-    // if (paymentData && paymentData.sessionId) {
-    //    const a = await UserModel.findByIdAndUpdate(userId, { subscriptionId: paymentData.sessionId });
-    // console.log(a)
-    // const paymentRecord = new PaymentModel({
-    //     userId: userId, // Assign the userId from UserModel
-    //     subscriptionId: paymentData.sessionId,
-    //     subscriptionType: subscriptionType,
-    //     subscribedDate: new Date(), // Set subscribedDate to current date
-    //     subscriptionExpirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) // Set subscriptionExpirationDate to 30 days from now
-    // });
-
-    // // Save the payment record
-    // await paymentRecord.save();
-    // console.log("Payment record saved:", paymentRecord);
-    // }
+    
 
     return paymentData?.data
 }
