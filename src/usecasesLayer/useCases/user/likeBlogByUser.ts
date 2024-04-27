e6e796edfbb0958ffc9f0fa6a851405fe775347f
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:25:43 +0530

    update: fixed bug

--- src/usecasesLayer/useCases/user/likeBlogByUser.ts
+++ src/usecasesLayer/useCases/user/likeBlogByUser.ts
@@ -1,8 +1,7 @@
 
 
-import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { Ilogger, Next } from "../../../infrastructureLayer/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
-import { ILogger } from "../../interface/services/IerrorLog";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
 
@@ -11,11 +10,11 @@ export const likeBlogByUser = async (
     userId: string,
     userRepository: IUserRepository,
     next: Next,
-    logger: ILogger
+    logger: Ilogger
 ) => {
     try {
        console.log('reached inside the latestBlog usecase engine ')
-       const response = await userRepository.likeBlog(blogId,userId, next)
+       const response = await userRepository.likeBlog(blogId,userId)
        return response
     } catch (error: unknown | never) {
         return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
