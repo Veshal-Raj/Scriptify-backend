cab8db20efd00f5bf4e8e991bf669b8879b41474
Veshal Raj <veshalraj1307@gmail.com>
2024-04-27 23:45:01 +0530

    refactor: formated the code and made some changes

--- src/AdaptersLayer/adminController.ts
+++ src/AdaptersLayer/adminController.ts
@@ -1,61 +1,87 @@
-import { Next, Req, Res } from "../infrastructureLayer/types/serverPackageTypes";
+import { Next, Req, Res, } from "../infrastructureLayer/types/serverPackageTypes";
 import { IAdminUseCase } from "../usecasesLayer/interface/usecase/adminUseCase";
 
-
-
 export class AdminController {
-    private readonly adminUseCase: IAdminUseCase;
+  private readonly adminUseCase: IAdminUseCase;
 
-    constructor(adminUseCase:IAdminUseCase) {
-        this.adminUseCase = adminUseCase
-    }
+  constructor(adminUseCase: IAdminUseCase) {
+    this.adminUseCase = adminUseCase;
+  }
 
-    async getAllUser(req: Req, res: Res, next: Next) {
-        try {
-            const result = await this.adminUseCase.getAllUser(next);
-            // console.log('result --->> ', result)
-            return res.status(200).json({ success: true, message: 'users have been fetched successfully', data: result})
-        } catch (error) {
-            throw error
-        }
+  async getAllUser(req: Req, res: Res, next: Next) {
+    try {
+      const result = await this.adminUseCase.getAllUser(next);
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "users have been fetched successfully",
+          data: result,
+        });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async changeUserStatus(req: Req, res: Res, next: Next) {
-        try {
-            const result = await this.adminUseCase.changeUserStatus(req, next);
-            console.log('result --->>>> ', result)
-            return res.status(200).json({ success: true, message: 'user status changed successfully', data: result})
-        } catch (error) {
-            throw error
-        }
+  async changeUserStatus(req: Req, res: Res, next: Next) {
+    try {
+      const userId = req.params.id;
+      const result = await this.adminUseCase.changeUserStatus(userId, next);
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "user status changed successfully",
+          data: result,
+        });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getAllBlogs(req: Req, res: Res, next: Next) {
-        try {
-            const result = await this.adminUseCase.getAllBlogs(next)
-            // console.log('result in controller --- ', result)
-            return res.status(200).json({ success: true, message: 'all blogs fetched successfully', data: result})
-        } catch (error) {
-            throw error
-        }
+  async getAllBlogs(req: Req, res: Res, next: Next) {
+    try {
+      const result = await this.adminUseCase.getAllBlogs(next);
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "all blogs fetched successfully",
+          data: result,
+        });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async changeBlogStatus(req: Req, res: Res, next: Next) {
-        try {
-            const blogId = req.body.blogId
-            const result = await this.adminUseCase.changeBlogStatus(blogId, next)
-            return res.status(200).json({ success: true, message: 'Blog status changed successfully', data: result})
-        } catch (error) {
-            throw error
-        }
+  async changeBlogStatus(req: Req, res: Res, next: Next) {
+    try {
+      const blogId = req.body.blogId;
+      const result = await this.adminUseCase.changeBlogStatus(blogId, next);
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "Blog status changed successfully",
+          data: result,
+        });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getAllReports(req: Req, res: Res, next: Next) {
-        try {
-            const result = await this.adminUseCase.getAllReports(next)
-            return res.status(200).json({ success: true, message: "all reports fetched successfully", data: result})
-        } catch (error) {
-            throw error
-        }
+  async getAllReports(req: Req, res: Res, next: Next) {
+    try {
+      const result = await this.adminUseCase.getAllReports(next);
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "all reports fetched successfully",
+          data: result,
+        });
+    } catch (error) {
+      throw error;
     }
+  }
 }
