418b34e3ebe044e7ced8e5bae0aa8059de7c10c8
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:04:12 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/findUserByEmail.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/findUserByEmail.ts
@@ -1,23 +1,15 @@
 import UserModel from "../../../models/userModel";
 
+export const findUserByEmail = async (email: string) => {
+  try {
+    const existingUser = await UserModel.findOne({
+      "personal_info.email": email,
+    });
 
-
-export const findUserByEmail = async(
-    email: string,
-   //  userModels: typeof UserModel
-) => {
-   try {
-     console.log('email in findUserByEmail in userRepository --->>>> ', email)
-   //   const allUsers = await UserModel.find()
-   //   console.log('all users --- ', allUsers)
-   console.log('admin@mail.com', 'actualstring')
-   console.log(email)
-        const existingUser = await UserModel.findOne({ 'personal_info.email': 'admin@mail.com' });
-        console.log(existingUser)
-        if (existingUser !== null && existingUser?.isVerified === false) throw new Error('User is blocked!!!') 
-        else return existingUser
-        
-   } catch (error) {
-        throw error
-   }
-}
+    if (existingUser !== null && existingUser?.isVerified === false)
+      throw new Error("User is blocked!!!");
+    else return existingUser;
+  } catch (error) {
+    throw error;
+  }
+};
