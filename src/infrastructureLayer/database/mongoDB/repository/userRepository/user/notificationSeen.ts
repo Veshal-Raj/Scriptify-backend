3928de4b1e7888024a1455bc6a7bf4da5598abe4
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:08:08 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/notificationSeen.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/notificationSeen.ts
@@ -1,23 +1,21 @@
 import NotificationModel from "../../../models/notificationModel";
 
+export const notificationSeen = async (notificationId: string) => {
+  try {
+    const updatedNotification = await NotificationModel.findByIdAndUpdate(
+      notificationId,
+      { seen: true },
+      { new: true } // Return the updated document
+    );
 
-
-export const notificationSeen = async ( notificationId: string ) => {
-    try {
-        const updatedNotification = await NotificationModel.findByIdAndUpdate(
-            notificationId,
-            { seen: true },
-            { new: true } // Return the updated document
-        );
-
-        // Check if the notification was found and updated
-        if (!updatedNotification) {
-            throw new Error('Notification not found');
-        }
-
-        // Return the updated notification
-        return updatedNotification;
-    } catch (error) {
-        throw error
+    // Check if the notification was found and updated
+    if (!updatedNotification) {
+      throw new Error("Notification not found");
     }
-}
+
+    // Return the updated notification
+    return updatedNotification;
+  } catch (error) {
+    throw error;
+  }
+};
