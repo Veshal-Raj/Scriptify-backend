ef08d6e8004cb53361da023f4be075178774742f
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:28:29 +0530

    update: fixed bug

--- src/usecasesLayer/useCases/user/trendingBlogs.ts
+++ src/usecasesLayer/useCases/user/trendingBlogs.ts
@@ -1,6 +1,5 @@
-import { Next } from "../../../infrastructureLayer/types/serverPackageTypes";
+import { Ilogger, Next } from "../../../infrastructureLayer/types/serverPackageTypes";
 import { IUserRepository } from "../../interface/repository/IuserRepository";
-import { ILogger } from "../../interface/services/IerrorLog";
 import { ErrorHandler } from "../../middlewares/errorHandler";
 
 
@@ -8,11 +7,11 @@ export const trendingBlogs = async (
     // blogRepository:
     userRepository: IUserRepository,
     next: Next,
-    logger: ILogger
+    logger: Ilogger
 ) => {
     try {
        console.log('reached inside the latestBlog usecase engine ')
-       const response = await userRepository.trendingBlogs(next)
+       const response = await userRepository.trendingBlogs()
        return response
     } catch (error: unknown | never) {
         return next(new ErrorHandler(500, error instanceof Error ? error.message : 'Unknown error', logger));
