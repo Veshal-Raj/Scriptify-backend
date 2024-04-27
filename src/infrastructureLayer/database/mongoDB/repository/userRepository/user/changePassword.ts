4cf8c97b8f01586c9f852e6be257b7b31dd43df9
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:00:54 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/changePassword.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/changePassword.ts
@@ -1,23 +1,17 @@
-import UserModel from "../../../models/userModel"
+import UserModel from "../../../models/userModel";
 
-
-
-export const changePassword = async(
-    userId: string,
-    newPassword: string
-) => {
-    try {
-        const user = await UserModel.findById(userId)
-        if (!user) {
-            throw new Error('User not found')
-        }
-        console.log('user data --- ', user)
-        console.log('newpassword -- ', newPassword)
-        user.personal_info.password = newPassword
-        await user.save()
-
-        return user
-    } catch (error) {
-        throw error
+export const changePassword = async (userId: string, newPassword: string) => {
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      throw new Error("User not found");
     }
-} 
+
+    user.personal_info.password = newPassword;
+    await user.save();
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
