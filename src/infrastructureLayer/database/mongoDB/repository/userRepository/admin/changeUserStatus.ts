730bff53e8ffac818ad498076ec54ee2e351270f
Veshal Raj <veshalraj1307@gmail.com>
2024-04-27 23:54:19 +0530

    refactor: foramtted the code

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/admin/changeUserStatus.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/admin/changeUserStatus.ts
@@ -1,21 +1,22 @@
 import IUser from "../../../../../../entitiesLayer/user";
 import UserModel from "../../../models/userModel";
 
+export const changeUserStatus = async (
+  userId: string
+): Promise<IUser | null> => {
+  try {
+    const user = await UserModel.findById(userId);
 
-export const changeUserStatus = async (id: string): Promise<IUser | null> => {
-    try {
-        const user = await UserModel.findById(id);
+    if (!user) return null;
 
-        if (!user) return null
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { $set: { isVerified: !user.isVerified } },
+      { new: true }
+    );
 
-        const updatedUser = await UserModel.findOneAndUpdate(
-            {_id: id},
-            {$set: {isVerified: !user.isVerified}},
-            {new: true}
-        )
-
-        return updatedUser
-    } catch (error) {
-        throw error
-    }
-}
+    return updatedUser;
+  } catch (error) {
+    throw error;
+  }
+};
