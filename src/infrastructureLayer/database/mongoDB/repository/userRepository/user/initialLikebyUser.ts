bfa4e4d832443bd6fab6b1046506ec298493a861
Veshal Raj <veshalraj1307@gmail.com>
2024-04-28 00:06:29 +0530

    refactor: removed unwanted code and formatted

--- src/infrastructureLayer/database/mongoDB/repository/userRepository/user/initialLikebyUser.ts
+++ src/infrastructureLayer/database/mongoDB/repository/userRepository/user/initialLikebyUser.ts
@@ -1,24 +1,24 @@
-
 import UserModel from "../../../models/userModel";
 
 export const initialLikebyUser = async (
-    userId: string,
-    blog_id: string,
-    userModel: typeof UserModel,
-   
+  userId: string,
+  blog_id: string,
+  userModel: typeof UserModel
 ): Promise<any | void> => {
-    try {
-        const user = await userModel.findById(userId);       
-        
-       
-        const isLiked = user?.userInteractions.userLikedBlogs.some(likedBlog => likedBlog.blogId.toString() === blog_id);
+  try {
+    const user = await userModel.findById(userId);
 
-        // Check if the blog is saved by the user
-        const isSaved = user?.userInteractions.userSavedBlogs.some(savedBlog => savedBlog.blogId.toString() === blog_id);
+    const isLiked = user?.userInteractions.userLikedBlogs.some(
+      (likedBlog) => likedBlog.blogId.toString() === blog_id
+    );
 
-        return { isLiked: isLiked, isSaved: isSaved };
+    // Check if the blog is saved by the user
+    const isSaved = user?.userInteractions.userSavedBlogs.some(
+      (savedBlog) => savedBlog.blogId.toString() === blog_id
+    );
 
-    } catch (error) {
-        throw error
-    }
-}
+    return { isLiked: isLiked, isSaved: isSaved };
+  } catch (error) {
+    throw error;
+  }
+};
